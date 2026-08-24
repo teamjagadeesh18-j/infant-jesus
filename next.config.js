704b2925f33841d009609d3b1c0caf4af45ae6fa
Jaga-098 <146536068+Jaga-098@users.noreply.github.com>
2026-08-24 22:42:16 +0530

    Fix Vercel Next.js framework config and outputFileTracingRoot

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname),
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 module.exports = nextConfig;
